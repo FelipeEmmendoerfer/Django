70b1029793781985fdb0b7c6b45f58a838174319
FelipeEmmendoerfer <felipe.emmendoerfer@gmail.com>
2026-07-28 23:34:13 -0300

    pagina de login, registro e recuperacao de senha

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, 'Login/login.html')
+
+
+def register(request):
+    return render(request, 'Login/register.html')
+
+
+def forgot_password(request):
+    return render(request, 'Login/forgot_password.html')
